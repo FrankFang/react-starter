36d45b5a81514b5955073cca875eb4b99fa2bcc2
frankfang <ff_online@outlook.com>
2023-11-01 23:13:32 +0800

    add Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+
 export const App: React.FC = () => {
   return (
     <div className="bg-pink-500 border border-black text-lg underline
     p-8 w-96 m-auto"
     >
       Hello world!
+      <Icon className="w-[1.2em] h-[1.2em] text-gray-500" icon="mdi-light:home" />
     </div>
   )
 }
