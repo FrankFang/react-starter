27baded878ef75d63de3b9e211573fafa3958ef0
frankfang <ff_online@outlook.com>
2023-11-01 23:20:31 +0800

    support dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
 
 export const App: React.FC = () => {
+  const onClick = () => {
+    document.documentElement.classList.toggle('dark')
+  }
   return (
     <div className="bg-pink-500 border border-black text-lg underline
-    p-8 w-96 m-auto"
+    p-8 w-96 m-auto dark:bg-black dark:text-white dark:border-white"
     >
       Hello world!
-      <Icon className="w-[1.2em] h-[1.2em] text-gray-500" icon="mdi-light:home" />
+      <Icon className="dark:text-blue-500 w-[1.2em] h-[1.2em] text-gray-500" icon="mdi-light:home" />
+      <button onClick={onClick}>Dark</button>
     </div>
   )
 }
